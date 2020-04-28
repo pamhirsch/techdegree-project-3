5d5ddc45ea443f4be7c56846ed847a5fca15ecf1
pamhirsch <pamelahirsch@mac.com>
2020-04-28 13:28:42 -0400

    Update script.js
    Updated script to show the other-title input field if Other is selected.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@ window.onload = function() {
   document.getElementById('name').focus();
 };
 
+const jobSelect = document.getElementById('title');
+
 // hide the "Other Job Role" input field at the beginning
 const otherJobInput = document.getElementById('other-title') ;
-
 if (otherJobInput) {
   otherJobInput.style.display = 'none';
 }
+
+jobSelect.addEventListener('change', (event) => {
+  if (event.target.value === "other") {
+    otherJobInput.style.display = 'inherit';
+  } else {
+    otherJobInput.style.display = 'none';
+  }
+});
